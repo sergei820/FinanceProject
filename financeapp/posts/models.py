e9e7563375a6ef7ab6e2a30aaef848026560508c
serje <serje@mayflower.work>
2025-11-04 10:41:46 +0200

    Add Posts DB model

--- financeapp/posts/models.py
+++ financeapp/posts/models.py
@@ -1,4 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
 
 class Post(models.Model):
     title = models.CharField('Title', max_length=100)
@@ -19,3 +28,12 @@ class Post(models.Model):
     class Meta:
         verbose_name = 'Post'
         verbose_name_plural = 'Posts'
+
+
+class PostLike(models.Model):
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('post_id', 'user_id')
