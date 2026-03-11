957794658216d363532062ed8b480f998e99fbe5
serje <serje@mayflower.work>
2026-03-11 22:46:14 +0200

    new website

---
+++ app/financeapp/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-9t(l0dw1rzp2494ozhfu86=xja)4yrnq^^8req3yjjn0%fu0dc"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
